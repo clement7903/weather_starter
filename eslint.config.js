aa63f908e61dfaa5ba54ec210c66679d744e5413
Clement <clement7903@hotmail.com>
2026-06-06 16:18:32 +0800

    make the astro non linted

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import tseslint from 'typescript-eslint';
 
 export default [
   {
-    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**', '**/docs/.astro/**'],
   },
   ...tseslint.configs.recommended,
   {
